cc7c6ee97454895fd3c77a03c8bfcdf48d8bfecb
khoanguyenminh222 <khoanguyenminh222@gmail.com>
2024-11-13 10:05:37 +0700

    update xem lịch họp với dữ liệu từ database

--- api/baseURL.js
+++ api/baseURL.js
@@ -1,5 +1,5 @@
 let domain = process.env.domain;
-let publicfolder = domain+"/uploads"
+let publicfolder = domain + "/uploads"
 const authRoute = {
     login: `${domain}/api/auth/login`,
     resetPassword: `${domain}/api/auth/resetPassword`,
@@ -65,84 +65,11 @@ const chiTietNhomChucNangRoute = {
     saveChucNangsForNhomChucNang: `${domain}/api/admin/chiTietNhomChucNang`,
 };
 
-const donViRoute = {
-    findAll: `${domain}/api/admin/donVi`,
-    findById: `${domain}/api/admin/donVi`, // /:id
-    create: `${domain}/api/admin/donVi`,
-    edit: `${domain}/api/admin/donVi`, // /:id body là donVi
-    delete: `${domain}/api/admin/donVi`, // /:id
-    findByIdCha: `${domain}/api/admin/donVi/idCha`, // /:idCha
-};
-
-
-
 const phanQuyenRoute = {
     getNhomChucNangByAccountId: `${domain}/api/admin/phanQuyen/nhomChucNang`, // truyền query accountId ?accountId=
     getChucNangByAccountId: `${domain}/api/admin/phanQuyen/chucNang`, // truyền query accountId ?accountId=
     savePhanQuyenForAccount: `${domain}/api/admin/phanQuyen`,
 };
-
-const cotBaoCaoRoute = {
-    getCotBaoCao: `${domain}/api/admin/cotBaoCao`, // truyền query baoCaoId ?baoCaoId=
-    addCotBaoCao: `${domain}/api/admin/cotBaoCao`,
-    updateCotBaoCao: `${domain}/api/admin/cotBaoCao`, // /:id
-    deleteCotBaoCao: `${domain}/api/admin/cotBaoCao`, // /:id
-    getCotCha: `${domain}/api/admin/cotBaoCao/cotCha`, // /:idCotCha
-    getAllCotBaoCaoDeGanChiTieu: `${domain}/api/admin/getAllCotBaoCaoDeGanChiTieu`, // truyền query baoCaoId ?baoCaoId=
-};
-
-const nhapBaoCaoRoute = {
-    getNhapBaoCaoByBaoCaoIdAndAccountId: `${domain}/api/admin/nhapBaoCao`, // truyền query baoCaoId ?baoCaoId=&accountId?
-    saveNhapBaoCao: `${domain}/api/admin/nhapBaoCao`,
-    saveChiTieuVaoBaoCao: `${domain}/api/admin/nhapBaoCao/saveChiTieuVaoBaoCao`,
-    deleteByBaoCaoIdAccountIdKyBaoCaoId: `${domain}/api/admin/nhapBaoCao`, // /truyền query baoCaoId ?baoCaoId=&accountId?
-    deleteByBaoCaoIdAccountId: `${domain}/api/admin/nhapBaoCao/deleteByBaoCaoIdAccountId`, // /truyền query baoCaoId ?baoCaoId=&accountId?
-    updateTrangThai: `${domain}/api/admin/nhapBaoCao`, // body: trangThai(báo cáo đã giao, đã nhập, đã gửi, xin đính chính, đang chỉnh sửa, báo cáo bị trả về), baoCaoId, accountId, xinSuaDuLieu
-    updateGuiLenDonViCha: `${domain}/api/admin/nhapBaoCao/guiLenDonViCha`, // body: guiLenDonViCha, accountId, baoCaoId
-    updateSauKhiGanChiTieuVaGiaoBaoCao: `${domain}/api/admin/nhapBaoCao/updateSauKhiGanChiTieuVaGiaoBaoCao`, // body: accountId, kyBaoCaoId, baoCaoId
-    getByBaoCaoId: `${domain}/api/admin/nhapBaoCao/getByBaoCaoId`, // query: baoCaoId
-    getByAccountId: `${domain}/api/admin/nhapBaoCao/getByAccountId`, // query: accountId
-};
-
-const baoCaoRoute = {
-    findAll: `${domain}/api/admin/baoCao`,
-    findActiveAll: `${domain}/api/admin/baoCaoActive`,
-    findById: `${domain}/api/admin/baoCao`, // /:id
-    create: `${domain}/api/admin/baoCao`,
-    edit: `${domain}/api/admin/baoCao`, // /:id body là baoCao
-    delete: `${domain}/api/admin/baoCao`, // /:id
-    updateTrangThai: `${domain}/api/admin/baoCao/updateTrangThai`, // /:id
-    getBaoCaoByTenBaoCao: `${domain}/api/admin/baoCao/tenBaoCao`, // /:tenBaoCao
-}
-
-const giaoBaoCaoRoute = {
-    findAll: `${domain}/api/admin/giaoBaoCao/all`,
-    findById: `${domain}/api/admin/giaoBaoCao`, // /:id
-    findByBaoCaoId: `${domain}/api/admin/giaoBaoCao`, //truyền query baoCaoId ?baoCaoId=
-    findByNguoiNhapId: `${domain}/api/admin/giaoBaoCao/nguoiNhapId`, // /:idNguoiNhap
-    create: `${domain}/api/admin/giaoBaoCao`,
-    edit: `${domain}/api/admin/giaoBaoCao`, // // /:id body là giaoBaoCao
-    delete: `${domain}/api/admin/giaoBaoCao`, // /:id
-}
-
-const cauHinhDonViNhanBaoCaoRoute = {
-    findAll: `${domain}/api/admin/cauHinhDonViNhanBaoCao/all`,
-    findByDonViNhanId: `${domain}/api/admin/cauHinhDonViNhanBaoCao/donViNhan`, // /:donViNhanId
-    create: `${domain}/api/admin/cauHinhDonViNhanBaoCao`,
-    delete: `${domain}/api/admin/cauHinhDonViNhanBaoCao`, // query ?donViNhanBaoCaoId=&donViGuiBaoCaoId=
-};
-
-const loaiKyBaoCaoRoute = {
-    findAll: `${domain}/api/admin/loaiKyBaoCao/all`,
-}
-
-const kyBaoCaoRoute = {
-    findAll: `${domain}/api/admin/kyBaoCao/all`,
-    create: `${domain}/api/admin/kyBaoCao`,
-    findById: `${domain}/api/admin/kyBaoCao`, // /:kyBaoCaoId
-    update: `${domain}/api/admin/kyBaoCao`, // /:kyBaoCaoId
-    delete: `${domain}/api/admin/kyBaoCao`, // /:kyBaoCaoId
-}
 
 const uploadFileRoute = {
     uploadFile: `${domain}/api/admin/upload`
@@ -156,67 +83,16 @@ const vaiTroRoute = {
     delete: `${domain}/api/admin/vaiTro`, // /:vaiTroId
 }
 
-const tienIchThongBaoRoute = {
-    findByNguoiNhanId: `${domain}/api/admin/tienIchThongBao/nguoiNhanId`, // /:nguoiNhanId
-    create: `${domain}/api/admin/tienIchThongBao`, // body: tienIchThongBao
-    update: `${domain}/api/admin/tienIchThongBao`, // /:id, body: tienIchThongBao
-    detele: `${domain}/api/admin/tienIchThongBao`, // /:id
-    sendMailAndSms: `${domain}/api/admin/tienIchThongBao/sendMailAndSms`, // body: tienIchThongBao
+const eventRoute = {
+    findAll: `${domain}/api/admin/event`,
+    create: `${domain}/api/admin/event`, // body là event
+    findById: `${domain}/api/admin/event`, // /:eventId
+    update: `${domain}/api/admin/event`, // /:eventId body là event
+    delete: `${domain}/api/admin/event`, // /:eventId
 }
 
-const quanLyApiKetNoiRoute = {
-    findAll: `${domain}/api/admin/quanLyApiKetNoi`,
-    create: `${domain}/api/admin/quanLyApiKetNoi`, // body quanLyApiKetNoi
-    findById: `${domain}/api/admin/quanLyApiKetNoi`, // /:id
-    update: `${domain}/api/admin/quanLyApiKetNoi`, // /:id, body quanLyApiKetNoi
-    delete: `${domain}/api/admin/quanLyApiKetNoi`, // /:id
-}
-
-const quanLyTokenKetNoiRoute = {
-    findAll: `${domain}/api/admin/quanLyTokenKetNoi`,
-    create: `${domain}/api/admin/quanLyTokenKetNoi`, // body quanLyTokenKetNoi
-    findById: `${domain}/api/admin/quanLyTokenKetNoi`, // /:id
-    update: `${domain}/api/admin/quanLyTokenKetNoi`, // /:id, body quanLyTokenKetNoi
-    delete: `${domain}/api/admin/quanLyTokenKetNoi`, // /:id
-}
-
-const quanLyPhanQuyenKetNoiRoute = {
-    findAll: `${domain}/api/admin/quanLyPhanQuyenKetNoi`,
-    create: `${domain}/api/admin/quanLyPhanQuyenKetNoi`, // body quanLyPhanQuyenKetNoi
-    findById: `${domain}/api/admin/quanLyPhanQuyenKetNoi`, // /:id
-    update: `${domain}/api/admin/quanLyPhanQuyenKetNoi`, // /:id, body quanLyPhanQuyenKetNoi
-    delete: `${domain}/api/admin/quanLyPhanQuyenKetNoi`, // /:id
-}
-
-const danhMucChucVuRoute = {
-    findAll: `${domain}/api/admin/danhMucChucVu`,
-    create: `${domain}/api/admin/danhMucChucVu`, // body danhMucChucVu
-    findById: `${domain}/api/admin/danhMucChucVu`, // /:id
-    update: `${domain}/api/admin/danhMucChucVu`, // /:id, body danhMucChucVu
-    delete: `${domain}/api/admin/danhMucChucVu`, // /:id
-}
-
-const danhMucQuocGiaRoute = {
-    findAll: `${domain}/api/admin/danhMucQuocGia`,
-    create: `${domain}/api/admin/danhMucQuocGia`, // body danhMucQuocGia
-    findById: `${domain}/api/admin/danhMucQuocGia`, // /:id
-    update: `${domain}/api/admin/danhMucQuocGia`, // /:id, body danhMucQuocGia
-    delete: `${domain}/api/admin/danhMucQuocGia`, // /:id
-}
-
-const danhMucDonViHanhChinhRoute = {
-    findAll: `${domain}/api/admin/danhMucDonViHanhChinh`,
-    create: `${domain}/api/admin/danhMucDonViHanhChinh`, // body danhMucDonViHanhChinh
-    findById: `${domain}/api/admin/danhMucDonViHanhChinh`, // /:id
-    update: `${domain}/api/admin/danhMucDonViHanhChinh`, // /:id, body danhMucDonViHanhChinh
-    delete: `${domain}/api/admin/danhMucDonViHanhChinh`, // /:id
-}
-
-const exportExcelMobileRoute = {
-    export: `${domain}/api/admin/exportExcelMobile`, // body headerRows, rowData, merges
-} 
-
-export {domain,
+export {
+    domain,
     publicfolder,
     authRoute,
     tokenRoute,
@@ -228,22 +104,7 @@ export {domain,
     nhomChiTieuRoute,
     chiTietNhomChucNangRoute,
     phanQuyenRoute,
-    cotBaoCaoRoute,
-    nhapBaoCaoRoute,
-    baoCaoRoute,
-    donViRoute,
-    giaoBaoCaoRoute,
     uploadFileRoute,
-    cauHinhDonViNhanBaoCaoRoute,
-    loaiKyBaoCaoRoute,
-    kyBaoCaoRoute,
     vaiTroRoute,
-    tienIchThongBaoRoute,
-    quanLyApiKetNoiRoute,
-    quanLyTokenKetNoiRoute,
-    quanLyPhanQuyenKetNoiRoute,
-    danhMucChucVuRoute,
-    danhMucQuocGiaRoute,
-    danhMucDonViHanhChinhRoute,
-    exportExcelMobileRoute,
+    eventRoute,
 };
--- screens/root/LichHopScreen.js
+++ screens/root/LichHopScreen.js
@@ -1,5 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Pressable, ScrollView, Linking } from 'react-native';
+import axiosInstance from '../../utils/axiosInstance';
+import { eventRoute, publicfolder } from '../../api/baseURL';
+import Toast from 'react-native-toast-message';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons/faDownload';
 
 // Hàm hỗ trợ để lấy ngày bắt đầu và ngày kết thúc của tuần hiện tại
 const getWeekDates = (date) => {
@@ -31,71 +36,30 @@ const getEventDays = (ngayBatDau, ngayKetThuc) => {
     return days;
 };
 
-// Dữ liệu sự kiện mẫu
-const events = [
-    {
-        ngayBatDau: '2024-11-12',
-        gioBatDau: '08:30',
-        ngayKetThuc: '2024-11-15',
-        gioKetThuc: '10:00',
-        noiDungCuocHop: 'Cuộc họp chiến lược',
-        chuTri: 'Ông A',
-        chuanBi: 'Phòng Kinh doanh',
-        thanhPhan: 'Ban Giám đốc, Phòng Kinh doanh',
-        moi: 'Giám đốc',
-        diaDiem: 'Phòng họp VTT',
-        ghiChu: 'Mang theo tài liệu dự án',
-        fileDinhKem: 'tai-lieu-hop.pdf',
-    },
-    {
-        ngayBatDau: '2024-11-13',
-        gioBatDau: '14:00',
-        ngayKetThuc: '2024-11-15',
-        gioKetThuc: '16:00',
-        noiDungCuocHop: 'Chuyển giao công việc',
-        chuTri: 'Ông B',
-        chuanBi: 'Phòng Nhân sự',
-        thanhPhan: 'Phòng Nhân sự, Phòng IT',
-        moi: 'Nhân viên Phòng IT',
-        diaDiem: 'Phòng họp VTT',
-        ghiChu: 'Chuẩn bị tài liệu liên quan',
-        fileDinhKem: 'chuyen-giao.pdf',
-    },
-    {
-        ngayBatDau: '2024-11-15',
-        gioBatDau: '09:00',
-        ngayKetThuc: '2024-11-15',
-        gioKetThuc: '12:00',
-        noiDungCuocHop: 'Sinh nhật công ty',
-        chuTri: 'Ban tổ chức',
-        chuanBi: 'Phòng Hành chính',
-        thanhPhan: 'Toàn bộ nhân viên',
-        moi: 'Toàn bộ nhân viên',
-        diaDiem: 'Hội trường VTT',
-        ghiChu: 'Ăn mặc trang trọng',
-        fileDinhKem: '',
-    },
-    {
-        ngayBatDau: '2024-11-15',
-        gioBatDau: '11:00',
-        ngayKetThuc: '2024-11-15',
-        gioKetThuc: '12:00',
-        noiDungCuocHop: 'Sinh nhật công ty',
-        chuTri: 'Ban tổ chức',
-        chuanBi: 'Phòng Hành chính',
-        thanhPhan: 'Toàn bộ nhân viên',
-        moi: 'Toàn bộ nhân viên',
-        diaDiem: 'Hội trường VTT',
-        ghiChu: 'Ăn mặc trang trọng',
-        fileDinhKem: '',
-    }
-];
-
-
 const CalendarPage = () => {
     const [currentWeek, setCurrentWeek] = useState(new Date());
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [currentWeekIndex, setCurrentWeekIndex] = useState(1);
+    const [events, setEvents] = useState([]);
+
+    const fetchEvents = async () => {
+        try {
+            const response = await axiosInstance.get(eventRoute.findAll);
+            setEvents(response.data);
+        } catch (error) {
+            console.log('Failed to fetch events:', error);
+            const errorMessage = error.response ? error.response.data.message : error.message;
+            Toast.show({
+                type: 'error',
+                text1: errorMessage,
+                position: 'top',
+                visibilityTime: 3000,
+            });
+        }
+    }
+    useEffect(() => {
+        fetchEvents();
+    }, []);
 
     const weekDates = getWeekDates(currentWeek);
 
@@ -113,34 +77,34 @@ const CalendarPage = () => {
             nextWeek.setDate(nextWeek.getDate() + 7);
             setCurrentWeek(nextWeek);
             setCurrentWeekIndex(2);
-    
+
             // Kiểm tra ngày hiện tại có nằm trong tuần mới không
             const today = new Date();
             const nextWeekDates = getWeekDates(nextWeek);
-    
+
             const focusDate = nextWeekDates.find(date => date.toDateString() === today.toDateString())
                 ? today
                 : nextWeekDates[0]; // Nếu không, focus vào thứ 2 của tuần mới
-    
+
             setSelectedDate(focusDate);
         }
     };
-    
+
     const handlePreviousWeek = () => {
         if (currentWeekIndex > 1) {
             const previousWeek = new Date(currentWeek);
             previousWeek.setDate(previousWeek.getDate() - 7);
             setCurrentWeek(previousWeek);
             setCurrentWeekIndex(1);
-    
+
             // Kiểm tra ngày hiện tại có nằm trong tuần trước đó không
             const today = new Date();
             const previousWeekDates = getWeekDates(previousWeek);
-    
+
             const focusDate = previousWeekDates.find(date => date.toDateString() === today.toDateString())
                 ? today
                 : previousWeekDates[0]; // Nếu không, focus vào thứ 2 của tuần mới
-    
+
             setSelectedDate(focusDate);
         }
     };
@@ -160,7 +124,7 @@ const CalendarPage = () => {
             // Tạo đối tượng Date giả định để so sánh giờ từ '00:00:00' cho mỗi sự kiện
             const timeA = new Date(`1970-01-01T${a.gioBatDau}:00`);
             const timeB = new Date(`1970-01-01T${b.gioBatDau}:00`);
-            
+
             // So sánh giờ giữa hai sự kiện
             return timeA.getTime() - timeB.getTime();
         });
@@ -210,24 +174,50 @@ const CalendarPage = () => {
                     <ScrollView showsVerticalScrollIndicator={false}>
                         {/* Hiển thị danh sách sự kiện trong ngày */}
                         {sortedEvents.map((event, index) => (
-                            <View key={index} className="bg-blue-100 p-4 mb-4 rounded-xl shadow-lg">
-                            <Text className="font-bold text-lg text-blue-900">{event.noiDungCuocHop}</Text>
-                            <Text className="text-blue-700">{event.diaDiem}</Text>
-                            <Text className="text-blue-500">{`Thời gian: ${event.gioBatDau} - ${event.gioKetThuc}`}</Text>
-                            <Text className="text-blue-600">{`Chủ trì: ${event.chuTri}`}</Text>
-                            <Text className="text-blue-600">{`Chuẩn bị: ${event.chuanBi}`}</Text>
-                            <Text className="text-blue-600">{`Thành phần: ${event.thanhPhan}`}</Text>
-                            <Text className="text-blue-600">{event.ghiChu}</Text>
-                            {/* Hiển thị nút tải nếu có file đính kèm */}
-                            {event.fileDinhKem && (
-                                <Pressable 
-                                    onPress={() => handleDownload(`/path/to/files/${event.fileDinhKem}`)}
-                                    className="mt-4 py-3 px-6 bg-blue-500 rounded-md"
-                                >
-                                    <Text style={{ color: '#fff', fontSize: 16 }}>Tải file đính kèm</Text>
-                                </Pressable>
-                            )}
-                        </View>
+                            <View key={index} className="bg-blue-100 p-6 mb-6 rounded-xl shadow-lg border border-blue-300">
+                                {/* Tên sự kiện */}
+                                <Text className="font-bold text-2xl text-blue-900 mb-2">{event.noiDungCuocHop}</Text>
+
+                                {/* Địa điểm */}
+                                <Text className="text-blue-700 text-xl mb-2 font-extrabold">{event.diaDiem}</Text>
+
+                                {/* Thời gian */}
+                                <Text className="text-blue-500 mb-2">Thời gian: <Text className="font-semibold text-xl">{event.gioBatDau} - {event.gioKetThuc}</Text></Text>
+
+                                {/* Chủ trì */}
+                                <Text className="text-blue-600 mb-2">Chủ trì: {event.chuTri}</Text>
+
+                                {/* Chuẩn bị (nếu có) */}
+                                {event.chuanBi && <Text className="text-blue-600 mb-2">Chuẩn bị: {event.chuanBi}</Text>}
+
+                                {/* Thành phần */}
+                                <Text className="text-blue-600 mb-2">Thành phần: {event.thanhPhan}</Text>
+
+                                {/* Mời (nếu có) */}
+                                {event.moi && <Text className="text-blue-600 mb-2">Mời: {event.moi}</Text>}
+
+                                {/* Ghi chú (nếu có) */}
+                                {event.ghiChu && <Text className="text-blue-600 mb-2">Ghi chú: {event.ghiChu}</Text>}
+
+                                {/* File đính kèm */}
+                                {event.fileDinhKem && (
+                                    <View className="mt-4">
+                                        <Text className="text-blue-800 font-semibold">File đính kèm</Text>
+                                        {JSON.parse(event.fileDinhKem).map((fileName, index) => (
+                                            <Pressable
+                                                key={index}
+                                                onPress={() => handleDownload(publicfolder + "/documents/" + fileName)}
+                                                className="py-2 px-4 mt-2 rounded-md bg-blue-500 hover:bg-blue-600"
+                                            >
+                                                <Text className="flex items-center text-white">
+                                                    <FontAwesomeIcon color='white' icon={faDownload} className="mr-2" /> {fileName}
+                                                </Text>
+                                            </Pressable>
+                                        ))}
+                                    </View>
+                                )}
+                            </View>
+
                         ))}
                     </ScrollView>
                 </View>
